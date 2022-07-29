ed23bf6685c5b032e0ca2640db58466051a3fb1d
profoswaldo <83677370+profoswaldo@users.noreply.github.com>
2022-07-29 10:59:11 -0300

    Encontro 05

---
+++ Linguagem de Programação Dispositivos Móveis/Encontro 05/Exemplo0003bLinearLayoutHorizontal/app/src/main/java/br/gov/exemplo0003b_linearlayouthorizontal/MainActivity.java
@@ -0,0 +1,14 @@
+package br.gov.exemplo0003b_linearlayouthorizontal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
