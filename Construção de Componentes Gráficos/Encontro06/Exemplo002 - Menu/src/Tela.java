35ab1f1572249b6e927668ddedcc63af24d46165
profoswaldo <83677370+profoswaldo@users.noreply.github.com>
2022-08-05 20:58:39 -0300

    Encontro06

---
+++ Construção de Componentes Gráficos/Encontro06/Exemplo002 - Menu/src/Tela.java
@@ -0,0 +1,83 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JSeparator;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Tela extends JFrame {
+
+	private JMenuBar menuBar;
+	private JMenu mnuAcademico;
+	private JMenu mnuAluno;
+	private JMenuItem mnuInclusao;
+	private JMenuItem mnuExclusão;
+	private JMenuItem mnuAlteracao;
+	private JMenuItem mnuConsulta;
+	private JSeparator separator;
+	private JMenuItem mnuSair;
+	private JButton btnSair;
+	
+	public Tela() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		mnuAcademico = new JMenu("Acadêmico");
+		menuBar.add(mnuAcademico);
+		
+		mnuAluno = new JMenu("Aluno");
+		mnuAcademico.add(mnuAluno);
+		
+		mnuInclusao = new JMenuItem("Inclusão");
+		mnuAluno.add(mnuInclusao);
+		
+		mnuExclusão = new JMenuItem("Exclusão");
+		mnuAluno.add(mnuExclusão);
+		
+		mnuAlteracao = new JMenuItem("Alteração");
+		mnuAluno.add(mnuAlteracao);
+		
+		mnuConsulta = new JMenuItem("Consulta");
+		mnuAluno.add(mnuConsulta);
+		
+		separator = new JSeparator();
+		mnuAcademico.add(separator);
+		
+		mnuSair = new JMenuItem("Sair");
+		mnuSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sair();
+			}
+		});
+		mnuAcademico.add(mnuSair);
+		
+		
+		btnSair = new JButton("Sair");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sair();
+			}
+		});
+		getContentPane().add(btnSair);
+		
+		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+	}
+
+	private void sair() {
+		JOptionPane.showMessageDialog(rootPane, "Até á próxima");
+		dispose();
+	}
+}
---
+++ Construção de Componentes Gráficos/Encontro06/Exemplo003 - Menu/src/Principal.java
@@ -0,0 +1,10 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Tela tela = new Tela();
+		tela.setVisible(true);
+	}
+
+}
